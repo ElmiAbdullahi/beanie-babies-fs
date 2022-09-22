d2dd31c1f3ad528bf32649f9e51e78c2eb7368d2
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-09-22 12:47:12 -0700

    set up import and export

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 /* Imports */
+import { getBeanies, getAstroSigns } from './fetch-utils.js';
+import { renderAstroSignOption, renderBeanie } from './render-utils.js';
 
 /* Get DOM Elements */
+const notificationDisplay = document.getElementById('notification-display');
+const searchForm = document.getElementById('search-form');
+const astroSignSelect = document.getElementById('astro-sign-select');
+const beanieList = document.getElementById('beanie-list');
 
 /* State */
-
+let error = null;
+let count = 0;
+let astroSigns = [];
+let beanies = [];
 /* Events */
 
 /* Display Functions */
---
+++ render-utils.js
@@ -0,0 +1,3 @@
+export function renderBeanie() {}
+
+export function renderAstroSignOption() {}
